88c054b2867ac927f83afc9b5d93b232c5415dd8
Elliot Alderson <blackman_5007@hotmail.com>
2020-03-16 17:18:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ClientList from "./ClientList";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
+  let clientInfo = { title: "Client List" };
+  let categoryInfo = { title: "Category List" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            {" "}
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            {" "}
+            <ClientList info={clientInfo} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
